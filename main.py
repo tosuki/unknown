c6dd61743792ba5bc505b9e7e96cdb876e49666a
tosuki <okaabe2006@gmail.com>
2025-05-13 08:25:31 -0300

    fix: fix issue related to button click checking

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from src.frame import Frame
 if __name__ == "__main__":
     game = Game(
         window=Window(1000, 800),
-        frame=Frame(10, 10, 600, 600)
+        frame=Frame(200, 100, 600, 600)
     )
     game.start()
--- src/entity/button.py
+++ src/entity/button.py
@@ -7,15 +7,17 @@ class Button(Entity):
         self.onclick = onclick
         self.text_color = text_color
 
-    def _is_clicked(self, x, y):
-        return (self.x <= x + self.width) and (self.y <= y <= self.y + self.height)
+    def _is_clicked(self, ctx, x, y):
+        bx, by = ctx.get_position(self.x, self.y)
+
+        return (bx <= x + self.width) and (by <= y <= by + self.height)
 
     def on_mouse_button_down(self, game, e):
         if e.button != 1:
             return
         x, y = e.pos
 
-        if self._is_clicked(x, y) and self.onclick:
+        if self._is_clicked(game.ctx, x, y) and self.onclick:
             self.onclick(game)
 
     def on_tick(self, game):
